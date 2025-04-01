ec7729b05558b5c60ab022a1bd94c7fbe0c5719a
Ajay Kumar Surada <ajaysurada123@gmail.com>
2025-03-31 22:30:32 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,6 @@
 import { useState } from "react";
 
 const projectsData = [
-            {
-      title: "Credit Card Report Analysis",
-      description: "The dashboard provides real-time insights into key performance metrics and trends, empowering stakeholders to make informed decisions.",
-      tags: ["PowerBI"],
-      links: {
-        github: "https://github.com/ajay-kumar2109/Credit-Card-Analysis",
-      },
-      image: "/certificates/PowerBI-Demo.gif"
-    },
       {
       title: "FMCG Mart Supply Chain Analysis",
       description: "This is end-to-end project of Supply Chain in Fast-Moving Consumer Goods(FMCG) domain, in which a real business problem is solved and provided useful insights on an interactive dashboard to the stakeholders using Power BI.",
@@ -46,15 +37,6 @@ const projectsData = [
       },
       image: "/certificates/supplychain.png"
     },
-  {
-      title: "MLOps Pipeline using Apache Airflow: Overview",
-      description: "The goal of this pipeline is to streamline the process of analyzing screentime data by automating its preprocessing and utilizing machine learning to predict app usage.",
-      tags: ["Apache Airflow"],
-      links: {
-        github: "https://github.com/ajay-kumar2109/MLOps-Pipeline-using-Apache-Airflow/tree/main?tab=readme-ov-file#overview",
-      },
-      image: "/certificates/airflow.png"
-    },
     {
       title: "Tableau Dashboards",
       description: "I created Tableau dashboards to analyze: 1. Employee Trends 2. Automotive Sales 3. Call Center Performance",
@@ -64,15 +46,6 @@ const projectsData = [
       },
       image: "/certificates/dashboard.png"
     },
-        {
-      title: "Movielytics Data Engineering Projects",
-      description: "The aim of this project, is to perform Extract, Transform, Load, on movies data, to answer questions the business may have about its users",
-      tags: ["Kaggle", "Amazon Ec2", "Apache Airflow", "Amazon Redshift"],
-      links: {
-        github: "https://github.com/ajay-kumar2109/movielytics_dataEngineering_Full-project",
-      },
-      image: "/certificates/movie.png"
-    },
     {
       title: "Retail Sales",
       description: "Real-time dashboard for monitoring ETL processes with live data streaming and interactive visualizations.",
@@ -81,42 +54,6 @@ const projectsData = [
         github: "https://github.com/ajay-kumar2109/retailSales",
       },
       image: "/certificates/sales1.png"
-    },
-    {
-      title: "healthcare-provider-fraud-detection",
-      description: "Automated CI/CD pipeline tool with custom workflows and cloud integrations.",
-      tags: [],
-      links: {
-        github: "https://github.com/ajay-kumar2109/healthcare-provider-fraud-detection",
-      },
-      image: "/certificates/healthcare.png"
-    },
-    {
-      title: "8-week-sql-coding-challenge",
-      description: "Advanced analytics platform with real-time data processing and visualization.",
-      tags: ["PostgreSQL"],
-      links: {
-        github: "https://github.com/ajay-kumar2109/8-week-sql-coding-challenge",
-      },
-      image: "/certificates/sql.png"
-    },
-    {
-      title: "Credit_Card_Report_Analysis",
-      description: "Comprehensive blockchain explorer with transaction tracking and smart contract analysis.",
-      tags: [],
-      links: {
-        github: "https://github.com/ajay-kumar2109/Credit_Card_Report_Analysis",
-      },
-      image: "/certificates/cc.png"
-    },
-    {
-      title: "Toronto-real-estate Analysis",
-      description: "Intelligent code completion and suggestion system using machine learning.",
-      tags: [],
-      links: {
-        github: "https://github.com/ajay-kumar2109/Toronto-real-estate",
-      },
-      image: "/certificates/toronto.png"
     }
   // Add more projects here
 ];
